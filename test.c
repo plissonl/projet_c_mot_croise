23be15936119a88b3c03be1c6b9511e7dab9cbd8
plissonl <louis.plisson@etu.unice.fr>
2018-04-21 21:59:41 +0200

    ajout de la méthode pour git (les lignes de commandes)

--- test.c
+++ test.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/*Fichier de test pour tester github*/
+//Demarche pour git
+/* git clone https://github.com/..... !!!! à faire une seule fois
+
+Se mettre dans le bon répertoire
+
+git pull     récupérer les fichiers à jour de github (à faire à chaque fois)
+
+
+git add *.c           ajoute les fichier .c au projet (évites d'ajouter les .e)
+git commit -m "message decrivant le commit"     fait la mise à jour des fichiers
+git push              partage le fichier sur github 
+
+faire ces 3 commandes à la suite 
+
+*/
+
 
 int main() {
 
