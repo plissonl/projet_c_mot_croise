9bf7c39d86b3ea968021f79ff4a244128a73aeca
emmanuelgarret <emmanuelgarret@orange.fr>
2018-06-04 17:41:15 +0200

    zone_noire_test1

--- data.h
+++ data.h
@@ -4,6 +4,16 @@
 #include<stdio.h>
 #include<libsx.h>
 #include<ctype.h>
+#include<callbacks.h>
+
+
+
+typedef struct{
+	char matrice_joueur[NB_LIGNES][NB_COLONNES];
+	char matrice_resultat[NB_LIGNES][NB_COLONNES];
+	int numFichier
+}ValeurCourante;
+
 
 
 
