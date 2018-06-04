fa20b4ceba500887b93633a4cad790ef6f82589b
emmanuelgarret <emmanuelgarret@orange.fr>
2018-06-04 18:37:40 +0200

    resolution erreurs_emmanuel

--- data.h
+++ data.h
@@ -11,19 +11,19 @@
 typedef struct{
 	char matrice_joueur[NB_LIGNES][NB_COLONNES];
 	char matrice_resultat[NB_LIGNES][NB_COLONNES];
-	int numFichier
+	int numFichier;
 }ValeurCourante;
 
 
 
 
-extern void init_display(int argc ,char **argv, char *d);
+extern void init_display(int argc ,char **argv,void *d);
 extern void init_matrice_joueur(ValeurCourante *data);
 
 
 
 
 /* Procédure initialisant la matrice appelée matrice_resultat */
-extern void init_matrice_resultat(ValeurCourante *d);
+extern void init_matrice_resultat(ValeurCourante *data);
 
 void afficherGrille(int taille,char mat[][taille]); //affichage de test
